1514bb1cf07e2065a83b95eb50ef8189566186c4
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-24 18:37:04 +0300

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ export default function App() {
       <Routes>
           <Route element={<Layout/>}>
             <Route path="/" element={<Home />}/>
-            <Route path="/about" element={<About />}/>
-            <Route path="/vans" element={<Vans />}/>
-            <Route path="/vans/:id" element={<VanDetail />}/>
+            <Route path="about" element={<About />}/>
+            <Route path="vans" element={<Vans />}/>
+            <Route path="vans/:id" element={<VanDetail />}/>
             
-            <Route path="/host" element={<HostLayout />}>
-              <Route path="/host" element={<Dashboard />}/>
-              <Route path="/host/income" element={<Income />}/>
-              <Route path="/host/reviews" element={<Reviews/>}/>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />}/>
+              <Route path="income" element={<Income />}/>
+              <Route path="reviews" element={<Reviews/>}/>
             </Route>
           </Route>
       </Routes>
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,12 +1,16 @@
-import React from "react";
-import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import React from "react"
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
+import Footer from "./Footer"
 
-export default function Layout(){
-    return(
-        <>
-            <Header/>
-            <Outlet/>
-        </>
+export default function Layout() {
+    return (
+        <div className="site-wrapper">
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     )
 }
