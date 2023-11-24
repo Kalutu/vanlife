271dd0c979827738b6798cd60f4e3ee341bf3421
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-24 10:28:41 +0300

    Project bootstraping

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from './Pages/About';
+import "./styles.css"
 
-function App() {
+export default function App() {
   return (
-    <>
-      Learn React
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+     
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+      </Routes>
+  </BrowserRouter>
   );
 }
 
-export default App;
