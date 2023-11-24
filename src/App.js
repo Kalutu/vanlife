357d64cc85dd846983c06ac01d97c0eb25b62c0a
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-24 11:12:11 +0300

    Added vans route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from './Pages/About';
+import Vans from "./Pages/Vans";
+import "./server"
 import "./styles.css"
 
 export default function App() {
@@ -11,12 +13,14 @@ export default function App() {
         <Link className="site-logo" to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
      
       <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
       </Routes>
   </BrowserRouter>
   );
---
+++ src/Pages/Vans.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Van() {
+    return (
+        <div className="home-container">
+            <h1>Vans page goes here</h1>    
+        </div>
+    )
+};
